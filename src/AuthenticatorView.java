394241b652e1f868006135d4cb6d491810f541ee
anthony-arseneau <anthonyfarseneau@icloud.com>
2024-03-23 00:31:07 -0300

    Moving folder

---
+++ src/AuthenticatorView.java
@@ -0,0 +1,129 @@
+package src;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * This class is for the authentication window for clients to first connect to the group chat
+ * @author Anthony Arseneau
+ * @version March 5, 2024
+ * Networks project
+ */
+public class AuthenticatorView extends JFrame {
+    // Class constants
+    private static final Color LIGHT_GREY = new Color(235, 235, 235); // Light grey color
+    private static final Font MAIN_FONT = new Font("Helvetica Neue", Font.PLAIN, 16); // The main font for the UI
+    private static final Font MAIN_FONT_BOLD = new Font("Helvetica Neue", Font.BOLD, 16); // The main font for the UI in bold
+    private static final Font TF_FONT = new Font("Monospcae", Font.PLAIN, 16); // The text field font for the UI
+    private static final int WINDOW_WIDTH = 275; // Width of the window
+    private static final int WINDOW_HEIGHT = 325; // Height of the window
+    private static final int MAX_WINDOW_WIDTH = 325; // Max width of the window
+    private static final int MAX_WINDOW_HEIGHT = 350; // Max height of the window
+    private static final int MAX_CHAR_USERNAME = 20; // Maximum length for the username
+    private static final int MAX_CHAR_PASSWORD = 20; // Maximum length for the password
+    private static final int BORDER_PADDING = 10; // Border padding around the main pane
+
+    // Instance variables
+    private JTextField tfUsername, tfPassword; // Textfield for the username and password input
+    private JLabel lbVerifying; // Verifying label
+
+    public void initialize() {
+        /********** Credentials Panel **********/
+        JLabel lbUsername = new JLabel("Enter username: ");
+        lbUsername.setFont(MAIN_FONT_BOLD);
+
+        tfUsername = new JTextField();
+        tfUsername.setFont(TF_FONT);
+        tfUsername.setBackground(LIGHT_GREY);
+        tfUsername.setDocument(new JTextFieldLimit(MAX_CHAR_USERNAME));
+
+        JLabel lbPassword = new JLabel("Enter password: ");
+        lbPassword.setFont(MAIN_FONT_BOLD);
+
+        tfPassword = new JTextField();
+        tfPassword.setFont(TF_FONT);
+        tfPassword.setBackground(LIGHT_GREY);
+        tfPassword.setDocument(new JTextFieldLimit(MAX_CHAR_PASSWORD));
+
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 1, 5, 2));
+        formPanel.setBackground(Color.WHITE);
+        formPanel.add(lbUsername);
+        formPanel.add(tfUsername);
+        formPanel.add(lbPassword);
+        formPanel.add(tfPassword);
+
+        /********** Welcome Label **********/
+        lbVerifying = new JLabel("", SwingConstants.CENTER);
+        lbVerifying.setFont(MAIN_FONT_BOLD);
+
+        /********** Buttons Panel *********/
+        MyButton btnClear = new MyButton("Clear");
+        btnClear.setFont(MAIN_FONT);
+        btnClear.setOpaque(true);
+        btnClear.setRolloverEnabled(true);
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                tfUsername.setText("");
+                tfPassword.setText("");
+                lbVerifying.setText("");
+            }
+        });
+
+        MyButton btnSubmit = new MyButton("Submit");
+        btnSubmit.setFont(MAIN_FONT);
+        btnSubmit.setOpaque(true);
+        btnSubmit.setRolloverEnabled(true);
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = tfUsername.getText();
+                String password = tfPassword.getText();
+                lbVerifying.setText("Verifying credentials...");
+                ChatClientListener chatClientListener = new ChatClientListener(username);
+                dispose();
+                chatClientListener.listenForMessages();
+                //chatClient.requestAccess();
+            }
+        });
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
+        buttonsPanel.add(btnClear);
+        buttonsPanel.add(btnSubmit);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(new EmptyBorder(BORDER_PADDING, BORDER_PADDING, BORDER_PADDING, BORDER_PADDING));
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(Color.WHITE);
+        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(lbVerifying, BorderLayout.CENTER);
+        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
+        setTitle("SPGC Authenticator");
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setMinimumSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        setMaximumSize(new Dimension(MAX_WINDOW_WIDTH, MAX_WINDOW_HEIGHT));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    /*
+    public void requestAccess(String username, String password) {
+        ChatClient chatClient = new ChatClient(username, password);
+        chatClient.listenForMessages();
+        ChatView chatView = new ChatView(chatClient);
+    }
+    */
+
+    public static void main(String[] args) {
+        AuthenticatorView clientFrame = new AuthenticatorView();
+        clientFrame.initialize();
+    }
+}
---
+++ src/ChatView.java
@@ -0,0 +1,147 @@
+package src;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * This class is for the chat window for clients to communicate with other authorized clients
+ * @author Anthony Arseneau
+ * @version March 6, 2024
+ * Networks project
+ */
+public class ChatView extends JFrame {
+    // Class constants
+    private static final Color RED = new Color(218, 37, 58); // Darker sea color
+    private static final Color LIGHTER_RED = new Color(222, 59, 78); // Darker sea color
+    private static final Color DARKER_RED = new Color(196, 33, 52); // Darker sea color
+    private static final Color LIGHT_BLUE = new Color(225, 235, 255); // Darker sea color
+    private static final Font MAIN_FONT = new Font("Helvetica Neue", Font.PLAIN, 16); // The main font for the UI
+    private static final Font MAIN_FONT_BOLD = new Font("Helvetica Neue", Font.BOLD, 16); // The main font for the UI in bold
+    private static final Font TF_FONT = new Font("Monospcae", Font.PLAIN, 16); // The text field font for the UI
+    private static final int WINDOW_WIDTH = 425; // Width of the window
+    private static final int WINDOW_HEIGHT = 600; // Height of the window
+    private static final int MAX_WINDOW_WIDTH = 650; // Max width of the window
+    private static final int MAX_WINDOW_HEIGHT = 750; // Max height of the window
+    private static final int MAX_CHAR_MESSAGE = 50; // Maximum length for the message to send
+    private static final int BORDER_PADDING = 10; // Border padding around the main pane
+    private static final int S_BORDER_PADDING = 5; // Border padding around the main pane
+
+
+    // Instance variables
+    private JTextField tfMessage; // Textfield for user to write their message
+    //private String message; // String value for message to send
+    private JTextArea textArea;
+    private boolean availableMessage;
+    private String message;
+    private ChatClientSender chatClientSender;
+    private Socket socket;
+    private String username;
+
+    public ChatView(Socket socket, String username) {
+        this.socket = socket;
+        this.username = username;
+    }
+
+    public void initialize() {
+        chatClientSender = new ChatClientSender(socket, username);
+        /********** Scroll Area **********/
+        textArea = new JTextArea(10, 15);
+        textArea.setBorder(new EmptyBorder(S_BORDER_PADDING, S_BORDER_PADDING, S_BORDER_PADDING, S_BORDER_PADDING));
+        textArea.setFont(TF_FONT);
+        textArea.setEditable(false);
+        textArea.setEnabled(false);
+        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        /********** Text Field Message & Send Button **********/
+        tfMessage = new JTextField();
+        tfMessage.setFont(TF_FONT);
+        tfMessage.setBackground(LIGHT_BLUE);
+        tfMessage.setDocument(new JTextFieldLimit(MAX_CHAR_MESSAGE));
+
+        MyButton btnSend = new MyButton("⇪"); // Interchangable with this symbole: ↵
+        btnSend.setForeground(Color.WHITE);
+        btnSend.setFont(MAIN_FONT_BOLD);
+        btnSend.setBorder(new EmptyBorder(1, 0, 3, 0));
+        btnSend.setBackground(RED);
+        btnSend.setHoverBackgroundColor(LIGHTER_RED);
+        btnSend.setPressedBackgroundColor(DARKER_RED);
+        btnSend.setRolloverEnabled(true);
+        btnSend.setFocusable(false);
+        btnSend.registerKeyboardAction(btnSend.getActionForKeyStroke(
+                                      KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, false)),
+                                      KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false),
+                                      JComponent.WHEN_FOCUSED);
+        btnSend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                message = tfMessage.getText();
+                if(!message.isBlank()) {
+                    chatClientSender.sendMessage(username + ": " + message);
+                }
+                tfMessage.setText("");
+            }
+        });
+
+        JPanel messagePanel = new JPanel();
+        messagePanel.setBackground(Color.GRAY);
+        messagePanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 0.8;
+        messagePanel.add(tfMessage, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 3;
+        c.gridy = 0;
+        c.weightx = 0.20;
+        messagePanel.add(btnSend, c);
+        
+        JPanel mainPanel = new JPanel();
+        
+        mainPanel.setBorder(new EmptyBorder(BORDER_PADDING, BORDER_PADDING, BORDER_PADDING, BORDER_PADDING));
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(Color.DARK_GRAY);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+        mainPanel.add(messagePanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
+        setTitle("Secure Private Group Chat");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setMinimumSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        setMaximumSize(new Dimension(MAX_WINDOW_WIDTH, MAX_WINDOW_HEIGHT));
+        setVisible(true);
+        getRootPane().setDefaultButton(btnSend);
+    }
+
+    public void addText(String message) {
+        textArea.append(message + "\n\r");
+    }
+    
+    public static void main(String[] args) throws IOException {
+        ChatView chatView = new ChatView(new Socket("127.0.0.1", 12000), "user1");
+        chatView.initialize();
+    }
+
+    public boolean hasAvailableMessage() {
+        return availableMessage;
+    }
+
+    public void setNoAvailableMessage() {
+        availableMessage = false;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    
+}
